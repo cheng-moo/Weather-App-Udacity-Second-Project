239b5a6ac96b50f4c23335c26642d50a8862701c
Mohamed <chengmooooo@gmail.com>
2022-07-28 14:12:43 +0200

    Prepare Server Side

---
+++ server.js
@@ -0,0 +1,22 @@
+// App endpoint
+const projectData = [];
+// Add express package to the project
+const express = require('express');
+// Create instance of our app with express;
+const app = express();
+// Dependencies
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// Middlewares
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+// Initializing the project folder
+app.use(express.static('website'));
+// our port
+const port = 8000;
+// Spin up the server
+const server = app.listen(port, () => {
+    console.log('Server Is Running');
+    console.log(`Local Host: ${port}`);
+});
