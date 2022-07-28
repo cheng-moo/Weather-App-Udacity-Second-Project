8538144e2f38c91e278075cdc9a92367052e0cfe
Mohamed <chengmooooo@gmail.com>
2022-07-29 00:14:49 +0200

    get data from server endpoint and update ui

--- website/app.js
+++ website/app.js
@@ -1,36 +1,28 @@
 
-
 const apiKey = '&appid=787af776a608e4e74df00f3b4b2fdfa3&units=imperial';
 
 // Generate button
 const generate = document.getElementById('generate');
 generate.addEventListener('click', getWeatherData);
 function getWeatherData () {
-    const zipCode = document.getElementById('zip').value;
-    const feelings = document.getElementById('feelings').value;
-    // API to get latitude and longitude from zipcode
-    const latLonBaseUrl = `http://api.openweathermap.org/geo/1.0/zip?zip=`;
-    // API to get the temp
-    const tempBaseUrl = `https://api.openweathermap.org/data/2.5/weather?`;
-    getData(latLonBaseUrl, zipCode, apiKey)
-    .then(data => {
-        // consume latitude and longitude info
-        const lat = data.lat;
-        const lon = data.lon;
-        // use lat and lon data to get temp info from the API
-        getData(tempBaseUrl, `lat=${lat}&lon=${lon}`, apiKey)
-        .then(tempData => {
-            // create a new Date Obj and take date info from it
+        const zipCode = document.getElementById('zip').value;
+        // API to get the temp from zipCode
+        const tempBaseUrl = `https://api.openweathermap.org/data/2.5/weather?zip=`;
+        getData(tempBaseUrl, zipCode, apiKey)
+        .then(data => {
+            console.log(data);
             const today = new Date();
-            const date = `${today.getDay()}/${today.getMonth() + 1 >= 10 ? `${today.getMonth() + 1}` : `0${today.getMonth() + 1}`}/${today.getFullYear()}`;
-            // get temp data in fahrenheit convert it to celsius
-            const celsiusTemp = Math.round((tempData.main.temp - 32) * 5/9);
-            console.log(celsiusTemp);
-            postData('/postWeatherData', {date: date, temp: celsiusTemp, feeling: feelings});
-        })
+            const date = `${today.getDate()}/${today.getMonth() + 1 >= 10 ? `${today.getMonth() + 1}` : `0${today.getMonth() + 1}`}/${today.getFullYear()}`;
+            const temp = Math.round((data.main.temp - 32) * 5/9);
+            const feeling = document.getElementById('feelings').value;
+            return {date, temp, feeling};
+        }).then(data => {
+        postData('/postWeatherData', data);
+        updateUI();
     })
-
 }
+
+
 // getData from API method
 const getData = async (url = '', data = '', apiKey = '') => {
     const response = await fetch(url + data + apiKey);
@@ -54,6 +46,21 @@ const postData = async (url = '', data = {}) => {
     try {
         const newData = await response.json();
         return newData;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+// updateUI
+const updateUI = async () => {
+    const response = await fetch('/getWeatherData');
+    try {
+        const weatherData = await response.json();
+        const date = document.getElementById('date');
+        const temp = document.getElementById('temp');
+        const content = document.getElementById('content');
+        date.textContent = weatherData[weatherData.length - 1].date;
+        temp.textContent =`${weatherData[weatherData.length - 1].temp}C`;
+        content.textContent = weatherData[weatherData.length - 1].feeling;
     } catch (error) {
         throw new Error(error);
     }
